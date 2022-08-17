596c4874f5e57b0699ba49773d00b32589d3b848
shvetsov-as <a002eshvetsov@gmail.com>
2022-08-17 23:45:48 +0300

    init commit

---
+++ src/test/java/com/shvetsov/AlbumApplicationTest.java
@@ -0,0 +1,15 @@
+package com.shvetsov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AlbumApplicationTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("******* Context loaded *******");
+    }
+}
