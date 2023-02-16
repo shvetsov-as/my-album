a606f956ea77cf767d4e1bd0e36c186b25d841da
shvetsov-as <a002eshvetsov@gmail.com>
2023-02-16 12:15:02 +0300

    jwt token provider added

---
+++ src/main/java/com/shvetsov/security/JWTTokenProvider.java
@@ -0,0 +1,69 @@
+package com.shvetsov.security;
+
+import com.shvetsov.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JWTTokenProvider {
+    public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+
+    public String generateToken(Authentication auth) {
+        User user = (User) auth.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);// token invalidate in [time now plus expiration time] from now till 10 minutes after
+        String userId = Long.toString(user.getId());
+        Map<String, Object> claimsMap = new HashMap<>();
+        claimsMap.put("id", userId);
+        claimsMap.put("username", user.getEmail());
+        claimsMap.put("firstname", user.getName());
+        claimsMap.put("lastname", user.getLastname());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .addClaims(claimsMap)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(SecurityConstants.SECRET)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException |
+                 MalformedJwtException |
+                 ExpiredJwtException |
+                 UnsupportedJwtException |
+                 IllegalArgumentException ex) {
+
+            LOG.error(ex.getMessage());
+            return false;
+        }
+    }
+
+    public Long getUserIdFromToken (String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
+    }
+}
